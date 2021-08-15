9512909f4b51ec8fd41e8c5f6ca82534466007e4
clk1006 <casparkiehn@icloud.com>
2021-08-15 14:03:11 +0200

    corrected API adress

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ let express=require('express');
 let fs=require('fs');
 const PORT=8080;
 let app=express();
-let messages=require('./messages.json');
-let i=0;
 app.use((req,res)=>{
     if(req.url=="/"){
         fs.readFile("index.html","utf8",(err,data)=>{
