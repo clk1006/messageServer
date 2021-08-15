c9ced585b8fe71b695a88a9da9b90e272d8972a9
clk1006 <casparkiehn@icloud.com>
2021-08-15 13:19:50 +0200

    massively changed the layout client-side

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let fs=require('fs');
 const PORT=8080;
 let app=express();
 let messages=require('./messages.json');
+let i=0;
 app.use((req,res)=>{
     if(req.url=="/"){
         fs.readFile("index.html","utf8",(err,data)=>{
@@ -13,17 +14,21 @@ app.use((req,res)=>{
             res.status(200).send(data);
         });
     }else if(req.url.includes("/getMessages")){
-        res.json(messages);
+        messages.unshift(i);
+        res.status(200).json(messages);
+        messages.shift();
     }else if(req.url.includes("/postMessage")){
         req=req.url.substr(13).split("&");
         messages.unshift({
             origin:req[0].split("=")[1],
             text:req[1].split("=")[1]
         });
-        messages=messages.slice(0,5);
+        messages=messages.slice(0,6);
         fs.writeFile("messages.json",JSON.stringify(messages), (err) => {
 			if (err) console.log(err);
 		});
+        res.status(200).json(messages);
+        i++;
     }else{
         res.status(404).send("404");
     }
