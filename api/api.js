82cdbfccd639e82e76adfcba5af74103d30f94a2
clk1006 <casparkiehn@icloud.com>
2021-08-18 17:52:46 +0200

    cretaed basic framework for moderation; deleting messages now requires a password stored in an environment variable

--- api/api.js
+++ api/api.js
@@ -1,25 +1,36 @@
+let fs=require ('fs');
 let messages={
-    i:0,
+    state:0,
     messages:[]
 }
 module.exports=(req,res)=>{
     if(req.query.type=="fetch"){
-        if(messages.i==req.query.i){
+        if(messages.state==req.query.i){
             res.status(200).json(messages.i);
         }else{
             res.status(200).json(messages);
         }
     }
-    else if(req.query.type=="delete"){
+    else if(req.query.type=="delete"&req.query.pw==process.env.admin_pw){
         messages.i=0;
         messages.messages=[];
         res.status(200).send("done");
     }
     else{
+        let id=messages.state;
+        while(messages.messages.reduce((pre,x)=>{
+            if(pre===x.id){
+                return true;
+            }
+            return pre;
+        },id)){
+            id++;
+        }
         let message={
             origin:req.query.origin,
-            text:req.query.text
-        }
+            text:req.query.text,
+            id:id
+        };
         messages.messages.unshift(message);
         res.status(200).json(messages);
         messages.i++;
