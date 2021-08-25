fae2006d1ad03e5bc5f3598dcf347275730c3bee
clk1006 <casparkiehn@icloud.com>
2021-08-25 18:18:30 +0200

    removed test.js, added first db code

--- api/api.js
+++ api/api.js
@@ -1,12 +1,14 @@
 const dbClient=require("./mongodb.js")
-let messages={
-    i:0,
-    messages:[]
+let storage={
+    messages:{
+        i:0,
+        messages:[]
+    },
+    tokens:[],
+    whiteList:[],
+    blackList:[],
+    filterType:""
 }
-let tokens=[];
-let whiteList=[];
-let blackList=[];
-let filterType="";
 const createToken=(tokens)=>{
     let token="";
     while(token.length<15){
@@ -17,24 +19,32 @@ const createToken=(tokens)=>{
     }
     return token;
 }
-module.exports=(req,res)=>{
+module.exports=async(req,res)=>{
+    const client=await dbClient;
+    const data=client.collection("data");
+    if((await data.find({init:1}).toArray()).length!=1){
+        data.insertOne({init:1});
+    }
+    else{
+        storage=await data.findOne({measages:{}});
+    }
     if(req.query.type=="fetch"){
-        if(messages.i==req.query.i){
-            res.status(200).json(messages.i);
+        if(storage.messages.i==req.query.i){
+            res.status(200).json(storage.messages.i);
         }else{
-            res.status(200).json(messages);
+            res.status(200).json(storage.messages);
         }
     }
     else if(req.query.type=="delete"){
         if(req.query.pw==process.env.admin_pw){
             if(req.query.ids=="all"){
-                messages.i=0;
-                messages.messages=[];
+                storage.messages.i=0;
+                storage.messages.messages=[];
             }
             else{
                 let ids=JSON.parse(req.query.ids);
-                messages.i++;
-                messages.messages=messages.messages.filter((x)=>{
+                storage.messages.i++;
+                storage.messages.messages=storage.messages.messages.filter((x)=>{
                     return (!ids.includes(x.id));
                 });
             }
@@ -45,16 +55,29 @@ module.exports=(req,res)=>{
         }
     }
     else if(req.query.type=="getToken"){
-        let token=createToken(tokens);
-        tokens.push(token);
+        let token=createToken(storage.tokens);
+        storage.tokens.push(token);
         res.status(200).send(token);
     }
     else if(req.query.type=="mod"){
         if(req.query.pw==process.env.admin_pw){
-            if(req.query.whiteList!=null) whiteList=req.query.whiteList;
-            if(req.query.blackList!=null) blackList=req.query.blackList;
-            if(req.query.filterType!=null) filterType=req.query.filterType
-            res.status(200).json([whiteList,blackList,filterType]);
+            if(req.query.whiteList!=null) storage.whiteList=req.query.whiteList;
+            if(req.query.blackList!=null) storage.blackList=req.query.blackList;
+            if(req.query.filterType!=null) storage.filterType=req.query.filterType;
+            res.status(200).json([storage.whiteList,storage.blackList,storage.filterType]);
+        }
+        else{
+            res.status(401).send("Request denied - wrong password");
+        }
+    }
+    else if(req.query.type=="readFile"){
+        if(req.query.pw==process.env.admin_pw){
+            fs.readFile("storage.json",(err,data)=>{
+                if(err){
+                    return;
+                }
+                res.status(200).send(data);
+            })
         }
         else{
             res.status(401).send("Request denied - wrong password");
@@ -63,24 +86,24 @@ module.exports=(req,res)=>{
     else{
         const token=req.query.token;
         const tokenF=(x)=>x==token;
-        if(tokens.includes(token)&&(filterType==""||filterType=="white"&&whiteList.includes(tokens.findIndex(tokenF))||filterType=="black"&&(!blackList.includes(tokens.findIndex(tokenF))))){
-            let id=messages.i;
+        if(storage.tokens.includes(token)&&(storage.filterType==""||storage.filterType=="white"&&storage.whiteList.includes(storage.tokens.findIndex(tokenF))||storage.filterType=="black"&&(!storage.blackList.includes(storage.tokens.findIndex(tokenF))))){
+            let id=storage.messages.i;
             const includesId=(arr,id)=>{
                 arrIds=arr.map((x)=>x.id);
                 return arrIds.includes(id);
             }
-            while(includesId(messages.messages,id)){
+            while(includesId(storage.messages.messages,id)){
                 id++;
             }
             let message={
                 origin:req.query.origin,
                 text:req.query.text,
                 id:id,
-                userID:tokens.findIndex(tokenF)
+                userID:storage.tokens.findIndex(tokenF)
             };
-            messages.messages.unshift(message);
+            storage.messages.messages.unshift(message);
             res.status(204).send();
-            messages.i++;
+            storage.messages.i++;
         }
         else{
             res.status(401).send("Request denied");
--- api/test.js
+++
@@ -1,5 +0,0 @@
-const dbClient=require('./mongodb.js')
-module.exports=async(req,res)=>{
-    const client=await dbClient;
-    res.status(200).json(client);
-}
